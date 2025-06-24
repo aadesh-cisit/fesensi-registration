10f3781abcdd9a941062d1082305e71fc5c10375
aadesh-cisit <aadesh.kumar@cisit.in>
2025-06-24 14:50:12 +0530

    int commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,44 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={``}
       >
-        {children}
+
+        <style>
+          {`
+          .branded-bg {
+            background-image: url('/bg.png');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+          }
+          .dark .branded-bg {
+            background-image: url('/bg.png');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+          }
+        `}
+        </style>
+        <div className="flex min-h-screen w-full">
+          {/* Left 1/3: Form/children */}
+          <div className="w-2/5 flex flex-col justify-center items-center bg-white z-10">
+            {children}
+          </div>
+          {/* Right 2/3: Image background */}
+
+
+          <div className="lg:border lg:border-gray-200 w-3/5  branded-bg flex flex-col ">
+            <div className="flex flex-col  lg:p-12 gap-3 mt-52">
+
+              <div className="flex flex-col pl-8 text-white">
+                <h1 className="text-7xl ">Welcome To </h1>
+                <span className=" text-7xl font-bold " >FESENSI</span>
+                <span className="font-semibold mt-5 tracking-[4px]">AI DRIVEN TECH SUPPORT</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </body>
     </html>
   );
