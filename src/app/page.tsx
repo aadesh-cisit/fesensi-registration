0742c4e3b2e5cbf9bc60297bb9a9f5c5fe62fc4b
aadesh-cisit <aadesh.kumar@cisit.in>
2025-06-24 16:03:08 +0530

    base code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold mb-4">Welcome to My App</h1>
+      <Button>button</Button>
       <p className="text-lg text-gray-700">This is a simple Next.js application.</p>
     </div>
   );
